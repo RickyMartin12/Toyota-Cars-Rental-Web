0a0199a960de8e1339354744516637a05f273938
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-15 18:56:11 +0000

    ola_total_99191121111211

--- admin/carro/action_carros_datatable.php
+++ admin/carro/action_carros_datatable.php
@@ -4,15 +4,20 @@
  
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/connect.php';
+ 
+use
+DataTables\Database,
+DataTables\Database\Query,
+DataTables\Database\Result;
 
 
-$RAW_SQL_QUERY="SELECT * FROM carros WHERE 1"; 
-$data = array();   
-while($row = mysqli_fetch_array($RAW_SQL_QUERY)) {
-   $data[] = $row;
-}
 
-header("Content-type: application/json");
-echo json_encode(array("data"=>$data,"options"=>'',"files"=>''));
+        $RAW_SQL_QUERY="SELECT * FROM carros WHERE 1";        
+        $r=$db->sql($RAW_SQL_QUERY)->fetchAll();
+
+        $arr=array("data"=>$r,"options"=>'',"files"=>'');//DATATABLE CLIENT SIDE PARSES
+        echo json_encode($arr);
+
+
 
 exit();
