1aed10207543d0c7fe2c7680dd87f6ad5e868a33
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-15 16:52:05 +0000

    ola3

--- conn.php
+++ conn.php
@@ -4,8 +4,8 @@ $servername = "containers-us-west-193.railway.app";
 $username = "root";
 $password = "wyGJjUpFOoD8FRzcWEk2";
 $dbname = "railway";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$port = "7728";
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 if (!$conn) {
     die("Erro ao ligar DB" . mysqli_connect_error());
 }
