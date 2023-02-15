1d6a3d493bea9ff523af2eabab6fd5feb9e6364d
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-15 16:40:46 +0000

    ola2

--- conn.php
+++ conn.php
@@ -1,7 +1,5 @@
 <?php
 
-date_default_timezone_set('Europe/Lisbon');//or change to whatever timezone you want
-
 $servername = "containers-us-west-193.railway.app";
 $username = "root";
 $password = "wyGJjUpFOoD8FRzcWEk2";
@@ -11,10 +9,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Erro ao ligar DB" . mysqli_connect_error());
 }
-
-
-
-header('Access-Control-Allow-Methods: POST');
 
 
 ?>
