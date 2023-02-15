1795bd6321531957a005c58c5541d3346f1598c1
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-15 18:46:35 +0000

    ola_total_991911211

--- admin/php/config.php
+++ admin/php/config.php
@@ -3,7 +3,6 @@
 
 require_once '../connect.php';
 
-
 // Enable error reporting for debugging (remove for production)
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -14,11 +13,11 @@ ini_set('display_errors', '1');
  */
 $sql_details = array(
 	"type" => "Mysql",  // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => $username,       // Database user name
-	"pass" => $password,       // Database password
-	"host" => $servername,       // Database host
-	"port" => "",       // Database connection port (can be left empty for default)
-	"db"   => $dbname,
+	"user" => "root",       // Database user name
+	"pass" => "wyGJjUpFOoD8FRzcWEk2",       // Database password
+	"host" => "containers-us-west-193.railway.app",       // Database host
+	"port" => "7728",       // Database connection port (can be left empty for default)
+	"db"   => "railway",
 	"dsn"  => ""        // PHP DSN extra information. Set as `charset=utf8` if you are using MySQL
 );
 
