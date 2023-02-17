c8e145e46564db0f8554e5470b2023b358a75f03
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-17 13:48:50 +0000

    database-ext-toyota-remove-htaccess-4

--- admin/modelo/action_datatable_modelo.php
+++ admin/modelo/action_datatable_modelo.php
@@ -1,26 +1,26 @@
 
 
 <?php
- 
- // DataTables PHP library
- 
- include $_SERVER['DOCUMENT_ROOT'] . "/admin/php/DataTables.php";
   
- use
- DataTables\Database,
- DataTables\Database\Query,
- DataTables\Database\Result;
- 
- 
- 
-         $RAW_SQL_QUERY="SELECT * FROM modelo WHERE 1";        
-         $r=$db->sql($RAW_SQL_QUERY)->fetchAll();
- 
-         $arr=array("data"=>$r,"options"=>'',"files"=>'');//DATATABLE CLIENT SIDE PARSES
-         
-         echo json_encode($arr);
- 
- 
- 
- exit();
- 
+// DataTables PHP library
+
+$servername = "containers-us-west-193.railway.app";
+$username = "root";
+$password = "wyGJjUpFOoD8FRzcWEk2";
+$dbname = "railway";
+$port = "7728";
+$RAW_SQL_QUERY="SELECT * FROM modelo WHERE 1";   
+$mysqli = new mysqli($servername, $username, $password, $dbname, $port);
+
+$result = $mysqli->query($RAW_SQL_QUERY);
+
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+$r = array();
+foreach ($rows as $row) {
+   $r[] = $row;
+}
+
+$arr=array("data"=>$r,"options"=>'',"files"=>'');//DATATABLE CLIENT SIDE PARSES
+echo json_encode($arr);
+
+exit();
