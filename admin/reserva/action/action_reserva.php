4ee41bfe1dbac3a35f69f5440d9032f9b8e3047b
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-21 13:33:27 +0000

    teste_email

--- admin/reserva/action/action_reserva.php
+++ admin/reserva/action/action_reserva.php
@@ -275,8 +275,9 @@ $attachment = $pdf->Output($filename,'S');
         $mail->isSMTP();  // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify mailgun SMTP servers
         $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'ricardopeleira16@gmail.com'; // SMTP username from https://mailgun.com/cp/domains
-        $mail->Password = 'npgnxhymkcxeoobc'; // SMTP password from https://mailgun.com/cp/domains
+        $mail->Username = 'ricardomanuelpeleira@gmail.com';
+        // Senha para SMTP authentication
+        $mail->Password = 'qcwrdlcthjenrtln';
         $mail->SMTPSecure = 'tls';   // Enable encryption, 'ssl'
                 $mail->Port= '587';
     
@@ -637,8 +638,9 @@ $attachment = $pdf->Output($filename,'S');
         $mail->isSMTP();  // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify mailgun SMTP servers
         $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'ricardopeleira16@gmail.com'; // SMTP username from https://mailgun.com/cp/domains
-        $mail->Password = 'npgnxhymkcxeoobc'; // SMTP password from https://mailgun.com/cp/domains
+        $mail->Username = 'ricardomanuelpeleira@gmail.com';
+        // Senha para SMTP authentication
+        $mail->Password = 'qcwrdlcthjenrtln';
         $mail->SMTPSecure = 'tls';   // Enable encryption, 'ssl'
                 $mail->Port= '587';
     
